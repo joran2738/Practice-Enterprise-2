324b29cb5dc0a7aa4497e562f94c76d5c7858bf2
RobinVdB8 <r0793034@student.thomasmore.be>
2024-02-25 13:25:53 +0100

    Added Qt Game Application Example

---
+++ Software/Qt/snake/game.cpp
@@ -0,0 +1,66 @@
+//This code is meant to be plain c code so it can be used on an STM32 microcontroller.
+
+#include <stdint.h>
+#include "debug.h"
+#include "game.h"
+
+uint8_t eventlist[EVENTSIZE];
+uint8_t eventindex = 0;
+uint32_t game_screen[SCREEN_WIDTH][SCREEN_HEIGHT];
+static point person = {SCREEN_WIDTH/2, SCREEN_HEIGHT/2};
+
+void init (void){
+    for(int x = 0; x < SCREEN_WIDTH; x++){
+        for(int y = 0; y < SCREEN_HEIGHT; y++){
+            game_screen[x][y] = 0xFF00FF00; //Green
+        }
+    }
+}
+
+void loop (void){
+    int key = readInput();
+    if(key == left){
+        person.x--;
+        if(person.x < 0) person.x = 0;
+    }
+    if(key == up){
+        person.y--;
+        if(person.y < 0) person.y = 0;
+    }
+    if(key == right){
+        person.x++;
+        if(person.x > SCREEN_WIDTH - 1) person.x = SCREEN_WIDTH - 1;
+    }
+    if(key == down){
+        person.y++;
+        if(person.y > SCREEN_HEIGHT - 1) person.y = SCREEN_HEIGHT - 1;
+    }
+    updateScreen();
+}
+
+int readInput()
+{
+    int newKey = 0;
+    if(eventindex > 0){
+        newKey = eventlist[0];
+        //QD << newKey << eventindex;
+        for(int i=0; i < eventindex; i++){
+            eventlist[i] = eventlist[i+1];
+        }
+        eventindex--;
+    }
+    return newKey;
+}
+
+void updateScreen()
+{
+    for(int x = 0; x < SCREEN_WIDTH; x++){
+        for(int y = 0; y < SCREEN_HEIGHT; y++){
+            if(x == person.x && y == person.y){
+                game_screen[x][y] = 0xFFFF0000; //red
+            }else{
+                game_screen[x][y] = 0xFF00FF00; //Green
+            }
+        }
+    }
+}
---
+++ Software/Qt/snake/mainwindow.cpp
@@ -0,0 +1,22 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QTimer>
+#include "game.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    auto *t = new QTimer(this);
+    t->setInterval(100);
+    connect(t, &QTimer::timeout,this,[=]{loop();ui->screen->update();});
+    t->start();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
