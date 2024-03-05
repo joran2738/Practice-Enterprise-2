8a172dad1fdd5563a882a7a79f1170b9617e6327
RobinVdB8 <r0793034@student.thomasmore.be>
2024-03-05 10:20:39 +0100

    Started on pong

--- Software/Qt/snake/game.cpp
+++ Software/Qt/snake/game.cpp
@@ -7,7 +7,8 @@
 uint8_t eventlist[EVENTSIZE];
 uint8_t eventindex = 0;
 uint32_t game_screen[SCREEN_WIDTH][SCREEN_HEIGHT];
-static point person = {SCREEN_WIDTH/2, SCREEN_HEIGHT/2};
+static point person = {SCREEN_WIDTH/2, SCREEN_HEIGHT - 5};
+static ballPoint ball = {(SCREEN_WIDTH)/2, SCREEN_HEIGHT - 6, pause};
 
 void init (void){
     for(int x = 0; x < SCREEN_WIDTH; x++){
@@ -21,20 +22,23 @@ void loop (void){
     int key = readInput();
     if(key == left){
         person.x--;
-        if(person.x < 0) person.x = 0;
-    }
-    if(key == up){
-        person.y--;
-        if(person.y < 0) person.y = 0;
+        if(person.x < BAR_SIZE/2) {
+            person.x = (BAR_SIZE/2);
+        }
     }
     if(key == right){
         person.x++;
-        if(person.x > SCREEN_WIDTH - 1) person.x = SCREEN_WIDTH - 1;
+        if(person.x > SCREEN_WIDTH - (BAR_SIZE/2) - 1) {
+            person.x = SCREEN_WIDTH - (BAR_SIZE/2) - 1;
+        }
     }
-    if(key == down){
-        person.y++;
-        if(person.y > SCREEN_HEIGHT - 1) person.y = SCREEN_HEIGHT - 1;
+    if(key == action) {
+        if (ball.dir == pause) {
+            ball.dir = NW;
+        }
     }
+    playBall();
+
     updateScreen();
 }
 
@@ -56,11 +60,113 @@ void updateScreen()
 {
     for(int x = 0; x < SCREEN_WIDTH; x++){
         for(int y = 0; y < SCREEN_HEIGHT; y++){
+            game_screen[x][y] = 0xFF00FF00; //Green
+        }
+    }
+
+    for(int x = 0; x < SCREEN_WIDTH; x++){
+        for(int y = 0; y < SCREEN_HEIGHT; y++){
             if(x == person.x && y == person.y){
-                game_screen[x][y] = 0xFFFF0000; //red
-            }else{
-                game_screen[x][y] = 0xFF00FF00; //Green
+                for(int i = x - BAR_SIZE/2; i <= x + BAR_SIZE/2; i++) {
+                    game_screen[i][y] = 0xFFFF0000; //red;
+                }
+            }
+            if(x == ball.x && y == ball.y) {
+                game_screen[x][y] = 0xFF0000FF;
             }
         }
+    }
+
+}
+
+void playBall() {
+    if (ball.y == 0) {
+        switch (ball.dir) {
+            case N:
+                ball.dir = S;
+                break;
+            case NW:
+                ball.dir = SW;
+                break;
+            case NE:
+                ball.dir = SE;
+                break;
+            default:
+                QD << "You shouldn't be here!";
+        }
+    }
+    if (ball.y == SCREEN_HEIGHT - 5) {
+        if (ball.x >= person.x - (BAR_SIZE/2) && ball.x <= person.x + (BAR_SIZE/2)) {
+            switch (ball.dir) {
+                case S:
+                    ball.dir = N;
+                    break;
+                case SE:
+                    ball.dir = NE;
+                    break;
+                case SW:
+                    ball.dir = NW   ;
+                    break;
+                default:
+                    QD << "You shouldn't be here!";
+            }
+        } else {
+            ball = {(person.x), SCREEN_HEIGHT - 6, pause};
+        }
+    }
+    if (ball.x == 0) {
+        switch (ball.dir) {
+            case NW:
+                ball.dir = NE;
+                break;
+            case SW:
+                ball.dir = SE;
+                break;
+            default:
+                QD << "You shouldn't be here!";
+        }
+    }
+    if (ball.x == SCREEN_WIDTH - 1) {
+        switch (ball.dir) {
+            case NE:
+                ball.dir = NW;
+                break;
+            case SE:
+                ball.dir = SW;
+                break;
+            default:
+                QD << "You shouldn't be here!";
+        }
+    }
+
+    switch (ball.dir) {
+        case N:
+            ball.y -= 1;
+            break;
+        case S:
+            ball.y += 1;
+            break;
+        case NE:
+            ball.y -= 1;
+            ball.x += 1;
+            break;
+        case NW:
+            ball.y -= 1;
+            ball.x -= 1;
+            break;
+        case SE:
+            ball.y += 1;
+            ball.x += 1;
+            break;
+        case SW:
+            ball.y += 1;
+            ball.x -= 1;
+            break;
+        case pause:
+            ball.y = ball.y;
+            ball.x = ball.x;
+            break;
+        default:
+            QD << "You shouldn't be here!";
     }
 }
--- Software/Qt/snake/game.h
+++ Software/Qt/snake/game.h
@@ -3,19 +3,28 @@
 
 //Configs
 #define EVENTSIZE 100
-#define SCREEN_WIDTH 160
-#define SCREEN_HEIGHT 120
+#define SCREEN_WIDTH 30
+#define SCREEN_HEIGHT 40
+#define BAR_SIZE 13
 
 //Function declaration
     void init (void);
     void loop (void);
     int readInput(void);
     void updateScreen(void);
+    void playBall(void);
 //Enums
     enum keys { left=1, up, right, down, action};
+    enum directions {N, NE, E, SE, S, SW, W, NW, pause};
 //Typedefs
     typedef struct {
         int x;
         int y;
     } point;
+
+    typedef struct {
+        int x;
+        int y;
+        directions dir;
+    } ballPoint;
 #endif // GAME_H
