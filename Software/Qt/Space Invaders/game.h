cf49fdc3d961fee1ccc54eaf35099e4dc469d57d
joran2738 <joranvannieuwenhoven@gmail.com>
2024-03-07 20:29:17 +0100

    got rid of code from brickbraker that's not need for spaceinvaders

--- Software/Qt/Space Invaders/game.h
+++ Software/Qt/Space Invaders/game.h
@@ -12,27 +12,14 @@
     void loop (void);
     int readInput(void);
     void updateScreen(void);
-    void playBall(void);
-    void moveBall(void);
 
 //Enums
     enum keys { left=1, up, right, down, action};
-    enum directions {N, NE, E, SE, S, SW, W, NW, pause};
+
 //Typedefs
     typedef struct {
         int x;
         int y;
     } point;
 
-    typedef struct {
-        int x;
-        int y;
-        directions dir;
-    } ballPoint;
-
-    typedef struct {
-        int x;
-        int y;
-        int visible;
-    } brick;
 #endif // GAME_H
