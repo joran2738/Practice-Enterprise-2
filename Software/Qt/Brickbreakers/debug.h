1fb93075340cd59ee17cc58e0fdad6e9cf3be1ba
RobinVdB8 <r0793034@student.thomasmore.be>
2024-03-13 13:35:50 +0100

    Solved compatibility problems space invaders & brickbreakers (hopefully)

--- Software/Qt/Brickbreakers/debug.h
+++
@@ -1,8 +0,0 @@
-#ifndef DEBUG_H
-#define DEBUG_H
-
-#include <QDebug>
-#define QD qDebug() << __FILE__ << __LINE__
-#define QDV(x) qDebug() << __FILE__ << __LINE__ << #x":" << x
-
-#endif // DEBUG_H
